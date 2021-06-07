5dcabca1c557148b5410cf14a716401aecd1b408
Johanna Andrango <johanna.andrango.b@student.ie.edu>
2021-06-07 13:36:53 +0200

    Reformat with black

--- src/ie_utils/__init__.py
+++ src/ie_utils/__init__.py
@@ -2,10 +2,14 @@
 IE Titanic utils.
 """
 
-__version__ = "0.1.0" #semver.org
+__version__ = "0.1.0"  # semver.org
+
+import pandas as pd
+
 
 def tokenize(text):
     return text.split()
+
 
 if __name__ == "__main__":
     print(tokenize("Hello world"))
